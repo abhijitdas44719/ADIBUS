54a6b070c670da7391e5762f57c82ce94159ead8
abhijitdas44719 <abhijitdas44719@gmail.com>
2025-07-05 11:14:57 +0530

    help.js

--- help.js
+++ help.js
@@ -603,3 +603,20 @@ const handleScroll = debounce(function() {
 }, 10);
 
 window.addEventListener('scroll', handleScroll);
+ document.addEventListener("DOMContentLoaded", function () {
+      const sections = document.querySelectorAll(".fade-in-section");
+
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+          } else {
+            entry.target.classList.remove("visible");
+          }
+        });
+      }, { threshold: 0.05 });
+
+      sections.forEach(section => {
+        observer.observe(section);
+      });
+    });
