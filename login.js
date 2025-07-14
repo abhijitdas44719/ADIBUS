b7266a8f7c4004be304608a991d644259af8458e
abhijitdas44719 <abhijitdas44719@gmail.com>
2025-07-14 19:57:34 +0530

    login.js

--- login.js
+++ login.js
@@ -501,3 +501,20 @@ document.addEventListener('DOMContentLoaded', () => {
     console.error('Initialization error:', error);
   }
 });
+   document.addEventListener("DOMContentLoaded", function () {
+      const sections = document.querySelectorAll(".fade-in-section");
+
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+          } else {
+            entry.target.classList.remove("visible");
+          }
+        });
+      }, { threshold: 0.05 });
+
+      sections.forEach(section => {
+        observer.observe(section);
+      });
+    });
